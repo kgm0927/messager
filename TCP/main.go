08c432283af1a3bc09a1adce9dda862eafa80bd2
kgm0927 <87554722+kgm0927@users.noreply.github.com>
2024-10-01 03:34:10 -0400

    main.go 함수 수정 ipv4 주소를 입력하여 원하는 주소로 접속할 수 있게 만듦.

--- TCP/main.go
+++ TCP/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
 	s := newServer()
 	go s.run()
+	fmt.Println("연결하고자 하는 주소를 입력해주세요.")
+	var ipv4 string
+	scanner := bufio.NewScanner(os.Stdin)
 
-	listener, err := net.Listen("tcp", ":8080")
+	// ipv4주소를 입력
+	if scanner.Scan() {
+		ipv4 = scanner.Text()
+		ipv4 += ":8080"
+		fmt.Println("입력한 주소:", scanner.Text())
+	}
+
+	listener, err := net.Listen("tcp", ipv4)
 	if err != nil {
 		log.Fatalf("unable to start server: %s", err.Error())
 	}
